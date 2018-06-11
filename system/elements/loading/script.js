840b0201094495e7d57e44df806f4311e19fff27
MikaylaFischler <micron1314@gmail.com>
2018-06-11 17:46:35 -0400

    updated elements

--- system/elements/loading/script.js
+++ system/elements/loading/script.js
@@ -1,1 +1,1 @@
-var __loading_html = '<div style="height:100px;width:auto;position:relative;"><div class="loading-spinner"></div></div>';
+var __loading_html = '<div style="height:100px;width:auto;position:relative;margin-left:auto;margin-right:auto;"><div class="loading-spinner"></div></div>';
